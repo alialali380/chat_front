eb970a0a1685e80ec76b2d106113e019caa3d715
alialali380 <0ali2ali40@gmail.com>
2025-06-13 19:52:48 +0300

    d

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,7 +1,7 @@
 // lib/socket.ts
 import { io, Socket } from 'socket.io-client';
 
-const URL = process.env.NEXT_PUBLIC_CHAT_API_URL || 'http://localhost:3000'; // استخدم المتغير بدون :3000
+const URL = `https://chat-back-ms9k.onrender.com`;
 
 export const socket: Socket = io(URL, {
   autoConnect: false,
