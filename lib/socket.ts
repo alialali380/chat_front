46297ae2232b470c7ac7c9b0894f5108c809edfa
alialali380 <0ali2ali40@gmail.com>
2025-06-13 18:44:43 +0300

    first commit

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,7 +1,7 @@
 // lib/socket.ts
 import { io, Socket } from 'socket.io-client';
 
-const URL = 'http://192.168.1.108:3000'; // عنوان خادم NestJS
+const URL = `http://${process.env.NEXT_PUBLIC_CHAT_API_URL}:3000`; // عنوان خادم NestJS
 // إنشاء اتصال مع سيرفر WebSocket 
 export const socket: Socket = io(URL, {
   autoConnect: false, // لن يتم الاتصال تلقائيًا بل عند طلب connect()
